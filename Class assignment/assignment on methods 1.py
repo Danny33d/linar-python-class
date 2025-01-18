5b1ea88792535d237937beaacaf2b35664c93375
DANNY <danielolagoroye45@gmail.com>
2025-01-18 07:27:05 -0800

    Update assignment on methods 1.py
    new diagonosis in line2

--- Class assignment/assignment on methods 1.py
+++ Class assignment/assignment on methods 1.py
@@ -1,5 +1,5 @@
 user_name = input("Enter user name")
-greet_user = ['HELLO AND HOW ARE YOU DOING']
+greet_user = ['HELLO AND HOW ARE YOU DOING'] #it will be i am doing great
 
 
 if user_name.startswith('Danny'):
